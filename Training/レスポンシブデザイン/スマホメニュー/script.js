75b6dff3688d995034c5cb6e75dbb3a926e4cd3d
pilot99fsa <132755045+pilot99fsa@users.noreply.github.com>
2024-02-18 19:07:34 +0900

    JavaScrip記述

--- Training/レスポンシブデザイン/スマホメニュー/script.js
+++ Training/レスポンシブデザイン/スマホメニュー/script.js
@@ -1,19 +1,19 @@
-class MobileMenu{
-    constructor(){
-    this.DOM={};
-    this.DOM.btn=document.querySelector('.mobile-menu__btn');
-    this.DOM.container=document.querySelector('#global-container');
-    this.eventType=this._getEventType();
-    //this._addEvent();
-  this.DOM.btn.addEventListener('click',this._toggle.bind(this));
-}
- 
- 
-_toggle(){
+class MobileMenu {
+  constructor() {
+    this.DOM = {};
+    this.DOM.btn = document.querySelector('.mobile-menu__btn');
+    this.DOM.container = document.querySelector('#global-container');
+    this.DOM.btn.addEventListener('click', this._toggle.bind(this))
+    // this.eventType = this._getEventType();
+    // this._addEvent();
+  }
+
+
+  _toggle() {
     this.DOM.container.classList.toggle('menu-open');
-    }
- // _addEvent(){
-     //   this.DOM.btn.addEventListener('click',this._toggle.bind(this))
-   //}
+  }
+  // _addEvent() {
+  //   this.DOM.btn.addEventListener('click', this._toggle.bind(this))
+  // }
 }
 new MobileMenu();
