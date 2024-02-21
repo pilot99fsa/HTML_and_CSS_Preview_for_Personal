d9adc8dba01e8c10c722213f14edde1936b7522c
pilot99fsa <132755045+pilot99fsa@users.noreply.github.com>
2024-02-21 16:33:24 +0900

    主政

--- Training/レスポンシブデザイン/スマホメニュー/script.js
+++ Training/レスポンシブデザイン/スマホメニュー/script.js
@@ -3,17 +3,22 @@ class MobileMenu {
     this.DOM = {};
     this.DOM.btn = document.querySelector('.mobile-menu__btn');
     this.DOM.container = document.querySelector('#global-container');
-    this.DOM.btn.addEventListener('click', this._toggle.bind(this))
-    // this.eventType = this._getEventType();
-    // this._addEvent();
+    this.eventType = this._getEventType()
+    this._addEvent()
   }
 
+
+  _getEventType() {
+    return window.ontouchstart ? 'touchstart' : 'click' // ブラウザにontotchstart(というプロパティ)が存在したら'_addEventはスマホでのタッチに動作が最適化される'
+    // 要修正 2024/2/21 16:00
+  }
 
   _toggle() {
     this.DOM.container.classList.toggle('menu-open');
   }
-  // _addEvent() {
-  //   this.DOM.btn.addEventListener('click', this._toggle.bind(this))
-  // }
+
+  _addEvent() {
+    this.DOM.btn.addEventListener('click', this._toggle.bind(this)) // bindメソッドでthisを束縛する
+  }
 }
 new MobileMenu();
